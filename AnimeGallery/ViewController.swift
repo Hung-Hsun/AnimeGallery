2095d4c5f035490760f0fab515cc0736f2a4a7e4
Hung-Hsun <hungsunlin@ntu.edu.tw>
2022-06-10 10:43:04 +0800

    Initial Commit

---
+++ AnimeGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeGallery
+//
+//  Created by 林宏勳 on 2022/6/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
